6e59f2c8f830bb19f6e4bd047eceaed31b4e5392
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2018-11-08 17:23:48 +0800

    settings activity

--- app/src/main/java/com/cogitator/livewall/MainActivity.kt
+++ app/src/main/java/com/cogitator/livewall/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.cogitator.livewall
 
-import android.support.v7.app.AppCompatActivity
+import android.app.AlertDialog
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        AlertDialog.Builder(this@MainActivity)
+            .setIcon(R.drawable.bats)
+            .setTitle("Live Wallpaper Settings")
+            .setMessage("All settings should be added here.")
+            .setPositiveButton("OK") { _, _ -> finish() }
+            .create().show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 }
